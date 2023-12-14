303107574e2e48978477c7bcb930f10f94368e28
oliviaWil <olivia@tribewilson.com>
2023-12-14 13:57:00 -0600

    updated dabase.js to support login

--- database.js
+++ database.js
@@ -1,10 +1,13 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('startup');
 const solutionCollection = db.collection('solutions');
+const userCollection = db.collection('user');
 // console.log("made it to database.js")
 
 // This will asynchronously test the connection and exit the process if it fails
@@ -16,6 +19,29 @@ const solutionCollection = db.collection('solutions');
   console.log(`Unable to connect to database with ${url} because ${ex.message}`);
   process.exit(1);
 });
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+  }
+  
+  function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+  }
+  
+  async function createUser(email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+  
+    return user;
+  }
+  
 
 async function addSolution(solution) {
     console.log('Adding solution to the database:', solution);
